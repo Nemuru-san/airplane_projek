d1eba0ac6388c8474785aaeb0d1cc092bdc9b3f5
Nemuru-san <nimemanta@gmail.com>
2021-09-20 13:03:55 +0700

    booking detail

--- lib/ui/pages/checkoutPage.dart
+++ lib/ui/pages/checkoutPage.dart
@@ -72,6 +72,90 @@ class checkoutPage extends StatelessWidget {
       );
     }
 
+    Widget bookingDetail() {
+      return Container(
+        margin: EdgeInsets.only(top: 30),
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 30,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(18),
+          color: ktextColorWhite,
+        ),
+        child: Column(
+          children: [
+            // destination title
+            Row(
+              children: [
+                Container(
+                  width: 70,
+                  height: 70,
+                  margin: EdgeInsets.only(right: 16),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(18),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage('assets/img_destination1.png'),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Kali Ciliwung',
+                        style: blackTextStyle.copyWith(
+                          fontSize: 18,
+                          fontWeight: medium,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        'Jakarta',
+                        style: greyTextStyle.copyWith(
+                          fontWeight: light,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 20,
+                      height: 20,
+                      margin: EdgeInsets.only(right: 2),
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(
+                            'assets/star_icon.png',
+                          ),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      '4.6',
+                      style: blackTextStyle.copyWith(
+                        fontWeight: medium,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+
+            // destination item detail
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: kbgColor,
       body: ListView(
@@ -80,6 +164,7 @@ class checkoutPage extends StatelessWidget {
         ),
         children: [
           route(),
+          bookingDetail(),
         ],
       ),
     );
