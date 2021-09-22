d9645a81dcba9d9defc1ee81db9b6abed032063f
Nemuru-san <nimemanta@gmail.com>
2021-09-22 21:42:12 +0700

    menambahkan app kedalam firebase

--- lib/ui/pages/mainPage.dart
+++ lib/ui/pages/mainPage.dart
@@ -1,6 +1,11 @@
+import 'package:airplane_projek/cubit/page_cubit.dart';
 import 'package:airplane_projek/ui/pages/homePage.dart';
+import 'package:airplane_projek/ui/pages/settingPage.dart';
+import 'package:airplane_projek/ui/pages/transactionPage.dart';
+import 'package:airplane_projek/ui/pages/walletPage.dart';
 import 'package:airplane_projek/ui/widgets/costumeButtonNav.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../shared/thame.dart';
 
 class mainPage extends StatelessWidget {
@@ -8,8 +13,19 @@ class mainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Widget buildContent() {
-      return homePage();
+    Widget buildContent(int currentIndex) {
+      switch (currentIndex) {
+        case 0:
+          return homePage();
+        case 1:
+          return transactionPage();
+        case 2:
+          return walletPage();
+        case 3:
+          return settingPage();
+        default:
+          return homePage();
+      }
     }
 
     Widget costumeButtonNavigation() {
@@ -26,16 +42,19 @@ class mainPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               costumeButtonNav(
+                index: 0,
                 imageUrl: 'assets/home_nav1.png',
-                isSelected: true,
               ),
               costumeButtonNav(
+                index: 1,
                 imageUrl: 'assets/booking_nav.png',
               ),
               costumeButtonNav(
+                index: 2,
                 imageUrl: 'assets/card_nav.png',
               ),
               costumeButtonNav(
+                index: 3,
                 imageUrl: 'assets/setting_nav.png',
               )
             ],
@@ -44,14 +63,18 @@ class mainPage extends StatelessWidget {
       );
     }
 
-    return Scaffold(
-      backgroundColor: kbgColor,
-      body: Stack(
-        children: [
-          buildContent(),
-          costumeButtonNavigation(),
-        ],
-      ),
+    return BlocBuilder<PageCubit, int>(
+      builder: (context, currentIndex) {
+        return Scaffold(
+          backgroundColor: kbgColor,
+          body: Stack(
+            children: [
+              buildContent(currentIndex),
+              costumeButtonNavigation(),
+            ],
+          ),
+        );
+      },
     );
   }
 }
--- lib/ui/widgets/costumeButtonNav.dart
+++ lib/ui/widgets/costumeButtonNav.dart
@@ -1,36 +1,53 @@
+import 'package:airplane_projek/cubit/page_cubit.dart';
 import 'package:airplane_projek/shared/thame.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class costumeButtonNav extends StatelessWidget {
+  final int index;
   final String imageUrl;
-  final bool isSelected;
 
   const costumeButtonNav({
     Key? key,
+    required this.index,
     required this.imageUrl,
-    this.isSelected = false,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        SizedBox(),
-        Container(
-          width: 24,
-          height: 24,
-          decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage(imageUrl))),
-        ),
-        Container(
-          width: 30,
-          height: 2,
-          decoration: BoxDecoration(
-              color: isSelected ? kprimaryColor : kTransparent,
-              borderRadius: BorderRadius.circular(18)),
-        )
-      ],
+    return GestureDetector(
+      onTap: () {
+        context.read<PageCubit>().setPage(index);
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(),
+          Image.asset(
+            imageUrl,
+            width: 24,
+            height: 24,
+            color: context.read<PageCubit>().state == index
+                ? kprimaryColor
+                : ktextColorGrey,
+          ),
+          // Container(
+          //   width: 24,
+          //   height: 24,
+          //   decoration: BoxDecoration(
+          //       image: DecorationImage(image: AssetImage(imageUrl))),
+          // ),
+          Container(
+            width: 30,
+            height: 2,
+            decoration: BoxDecoration(
+                color: context.read<PageCubit>().state == index
+                    ? kprimaryColor
+                    : kTransparent,
+                borderRadius: BorderRadius.circular(18)),
+          )
+        ],
+      ),
     );
   }
 }
