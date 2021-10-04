b512c8ddddc17395170b5f68dc08609a73d0c423
Nemuru-san <nimemanta@gmail.com>
2021-10-04 09:40:17 +0700

    menambahkan signup auth dan user service

---
+++ lib/models/userModel.dart
@@ -0,0 +1,27 @@
+import 'package:equatable/equatable.dart';
+
+class userModel extends Equatable {
+  final String id;
+  final String email;
+  final String name;
+  final String hobby;
+  final int balance;
+
+  userModel({
+    required this.id,
+    required this.email,
+    this.name = '',
+    this.hobby = '',
+    this.balance = 0,
+  });
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [
+        id,
+        email,
+        name,
+        hobby,
+        balance,
+      ];
+}
